5343a31f869dd3307bf4b2da126ca2cacfef067f
Tupui <23188539+tupui@users.noreply.github.com>
2026-03-20 11:51:34 +0200

    Add stellarpg to the featured section

--- dapp/src/constants/featuredProjectsConfigData.js
+++ dapp/src/constants/featuredProjectsConfigData.js
@@ -17,6 +17,23 @@ export const featuredProjectsConfigData = [
       "GD4FXNCYPQWNDWZYZZD4WFYYFTP466IKAKCZOYE5TPFTSSOZDA4QF3ER",
     ],
   },
+  {
+    projectName: "stellarpg",
+    projectFullName: "Stellar Community Fund - Public Goods Award",
+    projectType: "GENERIC",
+    logoImageLink:
+      "https://communityfund.stellar.org/_next/image?url=%2Fsvg%2FSCFLogo.svg&amp;w=96&amp;q=75",
+    description: "Stellar Community Fund Voting Platform for Public Goods Award",
+    organizationName: "Stellar Community Fund",
+    officials: {
+      websiteLink: "https://stellar.gitbook.io/scf-handbook/supporting-programs/public-goods-award",
+    },
+    socialLinks: {},
+    authorHandles: ["tupui"],
+    maintainersAddresses: [
+      "GBMOFLBLOINEUB6BTAF6U3PY6YYYMPZ5LT7R7VQCR6RXALBJZOJ4IYTW",
+    ],
+  },
 ];
 
 export function getFeaturedProjectsConfigData() {
